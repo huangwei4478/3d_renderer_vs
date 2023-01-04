1340ab11ddfe0d92ecb1570e0a3d41e2d1ac058f
huangwei4478 <huangwei4478@users.noreply.github.com>
2023-01-04 23:20:59 +0800

    [add] Draw Line & Draw Triangle

--- src/main.c
+++ src/main.c
@@ -115,9 +115,22 @@ void render(void) {
 	// Loop all projected triangles and render them
 	for (int i = 0; i < N_MESH_FACES; i++) {
 		triangle_t triangle = triangles_to_render[i];
+		
+		// Draw vertex points
 		draw_rect(triangle.points[0].x, triangle.points[0].y, 4, 4, 0xFF00FFFF);
 		draw_rect(triangle.points[1].x, triangle.points[1].y, 4, 4, 0xFF00FFFF);
 		draw_rect(triangle.points[2].x, triangle.points[2].y, 4, 4, 0xFF00FFFF);
+	
+		// Draw unfilled triangle
+		draw_triangle(
+			triangle.points[0].x,
+			triangle.points[0].y,
+			triangle.points[1].x,
+			triangle.points[1].y,
+			triangle.points[2].x,
+			triangle.points[2].y,
+			0xFF00FF00
+		);
 	}
 
 	render_color_buffer();
