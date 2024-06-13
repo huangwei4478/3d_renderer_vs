f089aec09f69c924fb3b8110fd71f642e0c7f67e
huangwei4478 <huangwei4478@users.noreply.github.com>
2024-06-13 22:12:26 +0800

    [add] back-face culling

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 
 // Array of triangles that should be rendered frame by frame
 triangle_t* triangles_to_render = NULL;
-vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
+vec3_t camera_position = { .x = 0, .y = 0, .z = 0 };
 
 float fov_factor = 640;
 
@@ -91,17 +91,41 @@ void update(void) {
 		// Loop all three vertices of this current face, 
 		// and apply transformation
 
+		vec3_t transformed_vertices[3];
+
 		for (int j = 0; j < 3; j++) {
 			vec3_t transformed_vertex = face_vertices[j];
 			transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
 			transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
 			transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
-		
+
 			// Translate the vertex away from the camera
-			transformed_vertex.z -= camera_position.z;
+			transformed_vertex.z += 5.0;
+
+			transformed_vertices[j] = transformed_vertex;
+		}
+
+
+		// Perform back-face culling
+		vec3_t vector_a = transformed_vertices[0];
+		vec3_t vector_b = transformed_vertices[1];
+		vec3_t vector_c = transformed_vertices[2];
+
+		vec3_t vector_ab = vec3_sub(vector_b, vector_a);
+		vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+
+		vec3_t normal = vec3_cross(vector_ab, vector_ac);
+		vec3_t camera_ray = vec3_sub(camera_position, vector_a);
+
+		float dot_normal_camera = vec3_dot(normal, camera_ray);
+
+		if (dot_normal_camera < 0) { continue; }
+
+
+		for (int j = 0; j < 3; j++) {
 
 			// Project the current vertex
-			vec2_t projected_point = project(transformed_vertex);
+			vec2_t projected_point = project(transformed_vertices[j]);
 			
 			// Scale and translate the projected points to the middle of the screen
 			projected_point.x += (SCREEN_WIDTH / 2);
